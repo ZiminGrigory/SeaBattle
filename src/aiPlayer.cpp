a65cff0b3871cab9039418096dc5afaa08a3a416
Liakh Ulia <sun_in_july@mail.ru>
2013-09-09 23:24:33 +0400

    aiPlayer was modified as abstract class

--- src/aiPlayer.cpp
+++ src/aiPlayer.cpp
@@ -5,72 +5,55 @@ AIPlayer::AIPlayer(View *view, QObject *parent):
 {
     myField.setPlr(NONE);
     enemyField.setPlr(YOU);
+    for (int i = 0; i < 3; i++)
+        attackedCells[i] = 0;
+    //success = false;
     //connect(this, SIGNAL(turnMade(int)), );
     qsrand(QTime::currentTime().msec());
 }
 
-void AIPlayer::installFleet()
+int AIPlayer::tryToKill(int id)
 {
-    for (int i = 0; i < fleet.size(); i++)
-    {
-        int shipSize = fleet[i]->size();
-        bool isHorizontal = true;
-        if (qrand() % 2 == 0)
-        {
-            isHorizontal == false;
-        }
-        int row = 0;
-        int col = 0;
-		int first = 0;
-		int second = 0;
-        FleetInstaller::PlacementStatus status = FleetInstaller::OK;
-        do
-        {
-            do
-            {
-                row = qrand() % FIELD_ROW_NUM;
-            }
-            while (!isHorizontal && row > FIELD_ROW_NUM - shipSize);
-            do
-            {
-                col = qrand() % FIELD_COL_NUM;
-            }
-            while(isHorizontal && col > FIELD_COL_NUM - shipSize);
+    // chose one of  neighboor cells
 
-            QPair<int, int> point1(row, col);
-            QPair<int, int> point2(row, col);
-            if (isHorizontal)
-            {
-                point2.second + shipSize;
-            }
-            else
-            {
-                point2.first + shipSize;
-            }
-			first = getIdByCoordinates(point1);
-			second = getIdByCoordinates(point2);
-
-            status = fleetInstaller->shipPlaced(first, second);
-            bool b = (status != FleetInstaller::OK) ||
-                    (status != FleetInstaller::HAVE_NOT_SHIP);
-            bool d = true;
-
-        } while((status != FleetInstaller::OK) &&
-                (status != FleetInstaller::HAVE_NOT_SHIP));
-    }
-}
-
-void AIPlayer::turn()
-{
-    int id = 0;
-    bool res = false;
+    int x = id / FIELD_ROW_NUM;
+    int y = id % FIELD_COL_NUM;
+    bool res = 0;
+    int orintation;
+    int nextAttacked = 0;
     do
     {
-        id = qrand() % (FIELD_ROW_NUM * FIELD_COL_NUM - 1);
-        res = enemyField.attack(id);
+        for (int i = 0; i < 3; i++)
+        {
+            if (attackedCells[i] == 0 ) //
+            {
+                nextAttacked = i;
+                orintation = i;
+            }
+        }
+        switch (nextAttacked)
+        {
+        case LEFT:
+            nextAttacked = id - 1;
+            break;
+        case DOWN:
+            nextAttacked = id + 10;
+            break;
+        case RIGHT:
+            nextAttacked = id  + 1;
+            break;
+        case HIGH:
+            nextAttacked = id - 10;
+            break;
+
+        }
+        res = enemyField.attack(nextAttacked); // check for cell were'nt visited earlyer
     }
     while (!res);
-    emit turnMade(id);
+    attackedCells[orintation] = nextAttacked;
+    direction = static_cast<Direction>(orintation); // remember attacking direction
+    return nextAttacked;
 }
+
 
 
