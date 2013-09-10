7aa377d5674e03760833c0fdab1f9f32ab2610d4
lahmatbiy <zimin_grigory@hotmail.com>
2013-09-11 00:23:08 +0400

    before merge

---
+++ client/src/gameLogics/textureAnalyzer.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "types.h"
+
+class TextureAnalyzer
+{
+public:
+	TextureAnalyzer();
+	Textures shipDamagedTexture(int i, int shipSize, bool orientation);
+	Textures shipTexture(int i, int shipSize, bool orientation);
+};
